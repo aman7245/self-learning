cd59c2f8b0a3f9ef4df465fdc6e5b20694647dcb
aman <aman@thb.co.in>
2020-04-22 17:32:28 +0530

    class component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './component/greet';
+import Welcome from './component/welcom';
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Welcome />
     </div>
   );
 }
